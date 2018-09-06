688abb2a4aa0651d288ae6f7b0dd9dc8024e6799
Vishal Tyagi <viishaltyagi@gmail.com>
2018-09-05 23:48:18 -0400

    Add files via upload

---
+++ Files/prog10.py
@@ -0,0 +1,16 @@
+inp = input('Enter a file name:')
+if inp == 'na na boo boo':
+    print('NA NA BOO BOO TO YOU - You have been punk''d!')
+else:
+    try:
+        fhand = open(inp)
+    except:
+        print('File does not exist:',inp)
+        quit()
+    count = 0
+    for i in fhand:
+        if not i.startswith('Subject'):
+            continue
+        else:
+            count += 1
+    print("There were", count, "subject lines in", inp)
